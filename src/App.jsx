0a8d0d8fb75aaf7bfd12ab3ff52cfc6dfd587e3a
ledangkhanhdut <ledangkhanhdut@gmail.com>
2025-07-05 14:56:37 +0700

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
-
+import Typography  from '@mui/material/Typography'
 
 function App() {
   return (
     <>
+    <Typography variant="h1" component="h2">
+      Trello Web App
+    </Typography>
+    <Typography variant="body1">
+      This is a simple Trello web app using Material UI.
+    </Typography>
+    <Typography variant="body2" color="textSecondary">
+      This is a secondary text.
+    </Typography>
       <div>Hello, Trello!</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
