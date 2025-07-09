6bc96a6d270ec9e7e29a1a00d0bc01ed607e1cd9
ledangkhanhdut <ledangkhanhdut@gmail.com>
2025-07-09 22:39:15 +0700

    Xử lý import file theo relative_absolute_import

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
-import theme from './theme.js'
-import App from './App.jsx'
+import theme from '~/theme.js'
+import App from '~/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
