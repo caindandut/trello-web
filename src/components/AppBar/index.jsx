1fe669c07a3e70bef47d9d3f14c81ed8236d6e6c
ledangkhanhdut <ledangkhanhdut@gmail.com>
2025-07-12 20:06:11 +0700

    responsive_appbar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -23,7 +23,9 @@ function AppBar() {
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
       alignItems: 'center',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
+      gap: 2,
+      overflow: 'auto'
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }} />
@@ -31,14 +33,16 @@ function AppBar() {
           <SvgIcon component={TrelloIcon} fontSize="small" inheritViewBox sx={{ color: 'primary.main' }} />
           <Typography variant="span" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>Trello</Typography>
         </Box>
-        <Workspace />
-        <Recent />
-        <Starred />
-        <Templates />
-        <Button variant="outlined">Create</Button>
+        <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', gap: 2, flexGrow: 1 }}>
+          <Workspace />
+          <Recent />
+          <Starred />
+          <Templates />
+          <Button variant="outlined">Create</Button>
+        </Box>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <TextField id="outlined-search" label="Search..." type="search" size='small' />
+        <TextField id="outlined-search" label="Search..." type="search" size='small' sx={{ minWidth: 120 }} />
         <ModeSelect />
         <Tooltip title="Notifications" sx={{ cursor: 'pointer' }}>
           <Badge color="secondary" variant="dot">
